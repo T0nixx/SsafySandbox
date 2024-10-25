ec3754bee273c01f5b4092043b03ff2225774e22
Setto1044 <108246779+Setto1044@users.noreply.github.com>
2024-10-25 13:48:22 +0900

    Delete sandbox directory

--- sandbox/src/main/java/com/ssafy/sandbox/todo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.sandbox.todo.repository;
-
-import com.ssafy.sandbox.todo.domain.TodoVo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<TodoVo, Integer> {
-}
