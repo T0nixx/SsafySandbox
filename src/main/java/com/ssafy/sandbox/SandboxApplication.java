b9d025431813c2f1aca3ea01f9dae3cbcd2c6304
T0nixx <jiseok95@gmail.com>
2024-10-28 14:52:41 +0900

    config: set CORS config

--- src/main/java/com/ssafy/sandbox/SandboxApplication.java
+++ src/main/java/com/ssafy/sandbox/SandboxApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SandboxApplication {
@@ -10,4 +13,13 @@ public class SandboxApplication {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://ssafysandbox.vercel.app");
+			}
+		};
+	}
 }
