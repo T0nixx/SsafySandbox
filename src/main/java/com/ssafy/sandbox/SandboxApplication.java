fb46090541d501e478af838ad93cdfbdd5fe2cd7
오지석 <jiseok95@gmail.com>
2024-11-04 22:05:35 +0900

    config: separate to WebConfig.java

--- src/main/java/com/ssafy/sandbox/SandboxApplication.java
+++ src/main/java/com/ssafy/sandbox/SandboxApplication.java
@@ -2,9 +2,6 @@ package com.ssafy.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SandboxApplication {
@@ -13,13 +10,4 @@ public class SandboxApplication {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://ssafysandbox.vercel.app").allowedMethods("GET", "POST", "PATCH", "DELETE");
-			}
-		};
-	}
 }
